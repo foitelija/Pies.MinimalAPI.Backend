8b58660d4104c33d8be771d566d535d0ac95a682
Foitelija <foitelija777@gmail.com>
2022-07-28 10:57:36 +0300

    feat: Database with CRUD & Usings to a separate file & Http to Https

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-using System.ComponentModel.DataAnnotations.Schema;
+
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDbContext<PiesDB>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MrMYSQL"));
+});
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,43 +12,77 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//проверяем, что БД создана
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<PiesDB>();
+    db.Database.EnsureCreated();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-var pies = new List<Pies>();
+
 
 app.UseHttpsRedirection();
-app.MapGet("/pies", () => pies);
-app.MapGet("/pies/{id}", (int id) => pies.FirstOrDefault(p => p.Id == id));
+app.MapGet("/pies", async (PiesDB db) => await db.Pies.ToListAsync());
 
-app.MapPost("pies", (Pies pie) => pies.Add(pie));
+app.MapGet("/pies/{id}", async (int id, PiesDB db) => await db.Pies.FirstOrDefaultAsync(p => p.Id == id) is Pies pies ? Results.Ok(pies) : Results.NotFound());
 
-app.MapPut("/pies", (Pies pie) =>
+app.MapPost("/pies", async([FromBody] Pies pies, PiesDB db) =>
 {
-    var index = pies.FindIndex(p => p.Id == pie.Id);
-    if(index < 0)
-    {
-        throw new Exception("Pie for update not found.");
-    }
-    pies[index] = pie;
+    db.Pies.Add(pies);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pies/{pies.Id}", pies);
 });
 
-app.MapDelete("/hotels/{id}", (int id) =>
+app.MapPut("/pies", async ([FromBody] Pies pie, PiesDB db) =>
 {
-    var index = pies.FindIndex(p=>p.Id == id);
-    if(index < 0)
+    var piesFromDb = await db.Pies.FindAsync(new object[]
     {
-        throw new Exception("Pie for delete not found.");
+        pie.Id
+    });
+    if(piesFromDb == null)
+    {
+        return Results.NotFound();
     }
-    pies.RemoveAt(index);
+    piesFromDb.Name = pie.Name;
+    piesFromDb.Description = pie.Description;
+    piesFromDb.Weight = pie.Weight;
+    piesFromDb.imageUrl = pie.imageUrl;
+    piesFromDb.Price = pie.Price;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
+app.MapDelete("/hotels/{id}", async (int id, PiesDB db) =>
+{
+    var piesFromDb = await db.Pies.FindAsync(new object[] { id });
+    if(piesFromDb == null)
+    {
+        return Results.NotFound();
+    }
+    db.Pies.Remove(piesFromDb);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+//Middleware HTTP -> HTTPs
+app.UseHttpsRedirection();
 
 app.Run();
 
+
+public class PiesDB : DbContext
+{
+    public PiesDB(DbContextOptions<PiesDB> options) : base(options) { }
+    public DbSet<Pies> Pies => Set<Pies>();
+}
 
 public class Pies
 {
